f2d4eabb4a8f223ae385bffcd9f5b51a447caead
Julio Villagrana <julio.villagrana.sanghelios2@gmail.com>
2023-03-28 07:57:36 -0600

    Add some

--- public/flutter_service_worker.js
+++ public/flutter_service_worker.js
@@ -301,9 +301,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "170bc518fcb273116667fc64c9c93d58",
-"/": "170bc518fcb273116667fc64c9c93d58",
-"main.dart.js": "4199f5f0ff4558c45798b9233c4f7d27",
+"index.html": "77206b34a73ed10dadcc14ce845e1cbe",
+"/": "77206b34a73ed10dadcc14ce845e1cbe",
+"main.dart.js": "09f37da3742ef37275b301a672139d6a",
 "manifest.json": "c8f65c56cbb2681d8a418544fc343768",
 "version.json": "041897a6572f3a85290971fa1afa65dc"
 };
