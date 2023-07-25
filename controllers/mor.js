ed9b1c72a16fdd2d0ab28fefbb59aaee3c28045a
Julio <julio.villagrana.sanghelios2@gmail.com>
2023-07-25 17:26:10 -0600

    Add some

--- controllers/mor.js
+++ controllers/mor.js
@@ -153,7 +153,7 @@ var controller = {
                     "_id":mongoose.Types.ObjectId(req.body.evento)
                 },
                 {
-                    $push:{"reservaciones.$[i].lista_invitados":mongoose.Types.ObjectId(req.body.reservacion)}
+                    $push:{"reservaciones.$[i].lista_invitados":mongoose.Types.ObjectId(req.uid)}
                 },
                 {
                     arrayFilters:[
