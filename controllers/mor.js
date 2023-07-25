2bd91b7378f0d7c8c44030f10ef2511d46ca469c
Julio <julio.villagrana.sanghelios2@gmail.com>
2023-07-25 00:14:06 -0600

    Add some

--- controllers/mor.js
+++ controllers/mor.js
@@ -39,9 +39,9 @@ var controller = {
 
             }
 
-            const checkUser = obj => obj._id === busqueda._id;
+            console.log(miPerfil.amigos.some(e=> e._id_usuario === busqueda._id));
 
-            if(miPerfil.amigos.some(checkUser)){
+            if(miPerfil.amigos.some(e=> e._id_usuario === busqueda._id)){
 
                 return res.status(200).json({ok:false,msg:"Este esta repetido",usuario:""});
             }
